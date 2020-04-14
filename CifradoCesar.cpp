254fa5bbf37fbd5d686be973e43c3490bb118af8
AARON <31245332+maniatik2017@users.noreply.github.com>
2020-04-13 19:32:06 -0500

    Update and rename example3.cpp to CifradoCesar.cpp

---
+++ CifradoCesar.cpp
@@ -0,0 +1,90 @@
+#include <fstream>
+#include <stdlib.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <string>
+using std::string;
+#include <sstream>
+using std::istringstream;
+using namespace std;
+
+
+class cesar{
+    private:
+        int a;
+        int clave=3;
+        string copia;
+        string mensaje;
+        string datos="abcdefghijklmnopqrstuvwxyz";
+
+    public:
+        cesar(string);
+        void cifrar();
+        void descifrar();
+        int formula(int);
+
+};
+
+cesar::cesar(string _mensaje){
+    mensaje=_mensaje;
+}
+int cesar::formula(int pocision){
+
+    int pocision_letra_cifrar;
+    int modulo=datos.length();
+
+    pocision_letra_cifrar=(pocision+clave)%26;
+    return pocision_letra_cifrar;
+
+
+
+}
+void cesar::cifrar(){
+    copia=mensaje;
+    int pocision;
+    cout<<"\nPalabra Cifrada: ";
+    for(int i=0;i<copia.length();i++){
+                a=datos.find(copia[i]);
+                if(a==-1)
+                    cout<<" ";
+                else{
+                    pocision=datos.find(copia[i]);
+                    copia[i]=datos[formula(pocision)];
+                    cout<<copia[i];
+                }
+    }
+    mensaje.erase(0);
+    cout<<"\n";
+}
+void cesar::descifrar(){
+   cout<<"Palabra Descifrada: ";
+   for(int i=0;i<copia.length();i++){
+                a=datos.find(copia[i]);
+                if(a==-1)
+                    cout<<" ";
+                else{
+                    if (a>=3){
+                        copia[i]=datos.at(a-clave);
+                        cout<<copia[i];
+                    }
+                    else if(a<3 && a>=0){
+                        copia[i]=datos.at(a-clave+datos.length());
+                        cout<<copia[i];
+                    }
+
+                }
+    }
+    copia.erase(0);
+    cout<<"\n";
+}
+int main(){
+    cesar palabra("zzzz zzz zzz");
+
+    palabra.cifrar();
+    palabra.descifrar();
+
+    return 0;
+}
+
